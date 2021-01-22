35120b23cf3f922f869d1a24e407a1b16f5fb8c2
arifjamal2012 <arifjamal2012@gmail.com>
2021-01-22 19:55:14 +0530

    react dashboard

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+const Header = () => {
+	return (
+		<div className='main_header'>
+			<h1>Dashboard management tool</h1>
+			<hr />
+		</div>
+	);
+};
+
+export default Header;
